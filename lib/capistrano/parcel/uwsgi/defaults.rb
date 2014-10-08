599d29db850e4ec4f86b0f6caecde25d16d96fe5
Tomas Barton <barton.tomas@gmail.com>
2014-10-08 09:16:11 +0200

    allow listening on http

--- lib/capistrano/parcel/uwsgi/defaults.rb
+++ lib/capistrano/parcel/uwsgi/defaults.rb
@@ -2,6 +2,7 @@
 set :use_uwsgi, true
 set :uwsgi_bin, '/usr/bin/uwsgi'
 set :uwsgi_user, 'www-data'
+set :uwsgi_listen, 'socket' # 'socket' or 'http'
 set :uwsgi_socket, "/run/uwsgi/app/#{fetch(:application)}/socket"
 set :uwsgi_processes, 1
 set :uwsgi_enable_threads, false
